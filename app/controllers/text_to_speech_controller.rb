006b331085f9600b29542255857ae8c8218c4a5b
john zajac <jzajac@enova.com>
2019-06-16 18:08:42 -0500

    text to speech debugging and added all radiohead albums favorite to view

--- app/controllers/text_to_speech_controller.rb
+++ app/controllers/text_to_speech_controller.rb
@@ -1,11 +1,19 @@
+require 'uri'
+
 class TextToSpeechController < ApplicationController
-
-  def speak(text_to_say)
-
-    speak_what = params[:text_to_say]
-
+  HOST = 'http://192.168.1.26'.freeze
+  PORT = '5005'.freeze # port of the sonos node api service
+  def speak
+    speak_what = params[:text_to_speak]
+    # speak_what = 'go to sleep'
+    puts "speak_what=#{speak_what}"
+    if !speak_what.present? then return end
+    speak_what_encoded = ERB::Util.url_encode(speak_what)
+    puts "speak_what_encoded=#{speak_what_encoded}"
+    uri = "#{HOST}:#{PORT}/sayall/#{speak_what_encoded}"
     begin
-      response = open("#{HOST}:#{PORT}/text_to_speech/#{speak_what}").read     
+      puts "uri=#{uri}"
+      response = open(uri).read     
     rescue ActionController::UnknownFormat => e
       if e.include? '204'
         # do nothing
